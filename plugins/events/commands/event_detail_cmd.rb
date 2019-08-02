61f62863f14ca32f8249818e284d64d5246f1790
HeroicLeigh <heroic.leigh@gmail.com>
2019-08-01 21:52:24 -0500

    Update event_detail_cmd.rb

--- plugins/events/commands/event_detail_cmd.rb
+++ plugins/events/commands/event_detail_cmd.rb
@@ -3,19 +3,19 @@ module AresMUSH
   module Events
     class EventDetailCmd
       include CommandHandler
-      
+
       attr_accessor :num
 
       def parse_args
         self.num = cmd.args ? cmd.args.to_i : nil
       end
-      
+
       def required_args
         [ self.num ]
       end
-      
+
       def handle
-        Events.with_an_event(self.num, client, enactor) do |event| 
+        Events.with_an_event(self.num, client, enactor) do |event|
           template = EventDetailTemplate.new(event, enactor)
           client.emit template.render
         end
