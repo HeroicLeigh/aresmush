052d6c6c51d8fa1ded6605f878cfe186c17b931a
HeroicLeigh <heroic.leigh@gmail.com>
2019-08-28 08:05:56 -0500

    Saving before upgrade.

--- plugins/chargen/custom_approval.rb
+++ plugins/chargen/custom_approval.rb
@@ -1,7 +1,15 @@
 module AresMUSH
   module Chargen
     def self.custom_approval(char)
-            
+  
+     case char.group("Faction")
+        when "Marines"
+          Channels.add_to_channels(nil, char, ["Marines"] )
+        when "Air Force"
+          Channels.add_to_channels(nil, char, ["Air Force"] )
+	when "Civilian"
+	  Channels.add_to_channels(nil, char, ["Civilians"] )
+    end            
       # If you don't want to have any custom approval steps, just leave this blank.
       
       # Otherwise, do what you need to do.  Here's an example of how to add
