872340775cc5367d2ad061466162fc6066bf9325
HeroicLeigh <heroic.leigh@gmail.com>
2019-09-02 14:51:22 -0500

    Update 0044_beta62_update.rb

--- install/migrations/0044_beta62_update.rb
+++ install/migrations/0044_beta62_update.rb
@@ -1,13 +1,13 @@
-module AresMUSH  
+module AresMUSH
 
   module Migrations
     class MigrationBeta62Update
       def require_restart
         true
       end
-      
+
       def migrate
-        
+
         Global.logger.debug "FS3 incapable rename."
         config = DatabaseMigrator.read_config_file("fs3skills_chargen.yml")
         if (!config['fs3skills']['allow_unskilled_action_skills'] )
@@ -15,7 +15,7 @@ module AresMUSH
           config['fs3skills'].delete 'allow_unskilled_action_skills'
           config = DatabaseMigrator.write_config_file("fs3skills_chargen.yml", config)
         end
-                               
+
         Global.logger.debug "Achievements."
         config = DatabaseMigrator.read_config_file("achievements.yml")
         if (!config['achievements']['achievements'])
@@ -31,7 +31,7 @@ module AresMUSH
         set_achievements("chargen.yml", "chargen", {
           'created_character' => { 'type' => 'story', 'message' => 'Created a character.'}
         })
-        
+
         set_achievements('events.yml', 'events', {
           "event_created" => { 'type' => 'community', 'message' => "Scheduled an event." }
         })
@@ -40,35 +40,35 @@ module AresMUSH
           "forum_reply" => { 'type' => 'community', 'message' => "Replied to a forum post." },
           "forum_post" => { 'type' => 'community', 'message' => "Created a forum post." }
         })
-        
+
         set_achievements('friends.yml', 'friends', {
           "friend_added" => { 'type' => 'community', 'message' => "Added a friend." }
         })
-        
+
         set_achievements('fs3combat_misc.yml', 'fs3combat', {
           "fs3_hero" => { 'type' => 'fs3', 'message' => "Heroed from a knockout." },
           "fs3_joined_combat" => { 'type' => 'fs3', 'message' => "Joined %{count} combats." }
         })
-        
+
         set_achievements('fs3skills_misc.yml', 'fs3skills', {
           "fs3_luck_spent" => { 'type' => 'fs3', 'message' => "Spent a luck point." },
           "fs3_roll"  => { 'type' => 'fs3', 'message' => "Rolled a skill." }
         })
-        
+
         set_achievements('profile.yml', 'profile', {
           "profile_edit" => { 'type' => 'portal', 'message' => "Edited your character profile." }
         })
-        
+
         set_achievements('scenes.yml', 'scenes', {
           "word_count" => { 'type' => 'story', 'message' => "Wrote %{count} words in scenes." },
           "scene_participant" => { 'type' => 'story', 'message' => "Participated in %{count} scenes." }
         })
-        
+
         set_achievements('website.yml', 'website', {
           "wiki_create" => { 'type' => 'portal', 'message' => "Created a wiki page." },
           "wiki_edit" => { 'type' => 'portal', 'message' => "Edited a wiki page." }
         })
-        
+
         if (File.exist?(File.join(AresMUSH.game_path, 'config', 'cookies.yml')))
           set_achievements('cookies.yml', 'cookies', {
             "cookie_given" => { 'type' => 'community', 'message' => "Gave a cookie." },
@@ -76,8 +76,8 @@ module AresMUSH
           })
         end
 
-        Manage.reload_config
-        
+        Global.config_reader.load_game_config
+
         Character.all.each do |c|
           c.achievements.each do |achievement|
             if (achievement.name =~ /fs3_joined_combat_/)
@@ -99,7 +99,7 @@ module AresMUSH
                 achievement.delete
               end
             end
-            
+
             if (achievement.name =~ /cookie_received_/)
               count = achievement.name.split('_').last.to_i
               Achievements.award_achievement(c, 'cookie_received', count)
@@ -107,15 +107,15 @@ module AresMUSH
             end
           end
         end
-      end 
-      
+      end
+
       def set_achievements(filename, section, data)
         config = DatabaseMigrator.read_config_file(filename)
         if (!config[section]['achievements'])
           config[section]['achievements'] = data
         end
         config = DatabaseMigrator.write_config_file(filename, config)
-        
+
       end
     end
   end
