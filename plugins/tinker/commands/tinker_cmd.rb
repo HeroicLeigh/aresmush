789f43f1b992fb29c6e96757f9e47f325650d37d
HeroicLeigh <heroic.leigh@gmail.com>
2019-09-09 08:41:08 -0500

    Saving before upgrade.

--- plugins/tinker/commands/tinker_cmd.rb
+++ plugins/tinker/commands/tinker_cmd.rb
@@ -7,7 +7,35 @@ module AresMUSH
         return t('dispatcher.not_allowed') if !enactor.has_permission?("tinker")
         return nil
       end
-      
+      Character.all.each do |c|
+          c.achievements.each do |achievement|
+            if (achievement.name =~ /fs3_joined_combat_/)
+              count = achievement.name.split('_').last.to_i
+              Achievements.award_achievement(c, 'fs3_joined_combat', count)
+              achievement.delete
+            end
+
+            if (achievement.name =~ /word_count_/)
+              count = achievement.name.split('_').last.to_i
+              Achievements.award_achievement(c, 'word_count', count)
+              achievement.delete
+            end
+
+            if (achievement.name =~ /scene_participant_/)
+              count = achievement.name.split('_').last.to_i
+              if (count > 0)
+                Achievements.award_achievement(c, 'scene_participant', count)
+                achievement.delete
+              end
+            end
+            
+            if (achievement.name =~ /cookie_received_/)
+              count = achievement.name.split('_').last.to_i
+              Achievements.award_achievement(c, 'cookie_received', count)
+              achievement.delete
+            end
+          end
+        end
       def handle
         client.emit_success "Done!"
       end
